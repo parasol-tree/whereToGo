c8d957365137286b5b56f1f406c6e55368421255
xiaoyu <1335963349@qq.com>
2018-01-10 23:15:20 +0800

    start a project

---
+++ src/pages/oneDayTour/index.vue
@@ -0,0 +1,15 @@
+<template>
+ 	<div>
+ 		oneDayTour
+ 	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'oneDayTour'
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import HomePage from '@/pages/homePage'
+import City from '@/pages/city'
+import Detail from '@/pages/detail'
+import OneDayTour from '@/pages/oneDayTour'
+import Order from '@/pages/order'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/city',
+      name: 'City',
+      component: City
+    },
+    {
+      path: '/detail',
+      name: 'Detail',
+      component: Detail
+    },
+    {
+      path: '/oneDayTour',
+      name: 'OneDayTour',
+      component: OneDayTour
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: Order
+    }
+  ]
+})
