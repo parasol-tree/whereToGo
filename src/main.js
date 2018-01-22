712a5a689bdb24d10b64a962072293f249d666be
jzxdd <569005169@qq.com>
2018-01-22 08:37:10 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from '@/store/'
 import FastClick from 'fastclick'
 
 import 'assets/style/reset.css'
@@ -18,6 +18,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
